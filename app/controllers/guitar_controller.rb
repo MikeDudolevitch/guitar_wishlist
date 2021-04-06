c628b93ce6d779fb1d588979476919f2b4e24496
MikeDudolevitch <mikedudolevitch@gmail.com>
2021-04-05 20:35:47 -0400

    fewer db calls in guitar controller, checked signup email is unique

--- app/controllers/guitar_controller.rb
+++ app/controllers/guitar_controller.rb
@@ -14,17 +14,14 @@ class GuitarController < ApplicationController
     post '/guitars' do
         redirect_if_not_logged_in
         photo_url = !params[:image_url].empty? ? params[:image_url] : "https://i.pinimg.com/originals/0b/0d/bc/0b0dbce265be5db9bf7061b4e8dc83f4.png"
-        guitar = Guitar.new(maker: params[:maker], model: params[:model], color: params[:color], year_created: params[:year_created], image_url: photo_url)
-        guitar.user_id = current_user.id
-    
+        @guitar = Guitar.new(maker: params[:maker], model: params[:model], color: params[:color], year_created: params[:year_created], image_url: photo_url)
+        @guitar.user_id = current_user.id
 
             if  guitar.save
                 redirect '/guitars'
             else
                 redirect '/guitars/error'
             end
-
-            
     end
 
     get '/guitars/error' do
@@ -32,39 +29,38 @@ class GuitarController < ApplicationController
     end
 
    get '/guitars/:id' do
-    redirect_if_not_authorized
     @guitar = Guitar.find_by(id: params[:id])
+    redirect_if_not_authorized
     erb :'guitars/show'
    end
 
    get '/guitars/:id/edit' do
-    redirect_if_not_authorized
     @guitar = Guitar.find_by(id: params[:id])
+    redirect_if_not_authorized
     erb :'/guitars/edit'
    end
 
    patch '/guitars/:id' do
+    @guitar = Guitar.find_by(id: params[:id])
     redirect_if_not_authorized
-    guitar = Guitar.find_by(id: params[:id])
-    if guitar.update(maker: params[:maker], model: params[:model], color: params[:color], year_created: params[:year_created], image_url: params[:image_url])
-        redirect "/guitars/#{guitar.id}"
+    if @guitar.update(maker: params[:maker], model: params[:model], color: params[:color], year_created: params[:year_created], image_url: params[:image_url])
+        redirect "/guitars/#{@guitar.id}"
     else
-        redirect "/guitars/#{guitar.id}/edit"
+        redirect "/guitars/#{@guitar.id}/edit"
    end
 end
 
     delete '/guitars/:id' do
+        @guitar = Guitar.find_by(id: params[:id])
         redirect_if_not_authorized
-        guitar = Guitar.find_by(id: params[:id])
-        guitar.destroy
+        @guitar.destroy
         redirect :'/guitars'
     end
 
     private
 
     def redirect_if_not_authorized
-        guitar = Guitar.find_by_id(params[:id])
-        if guitar.user_id != session["user_id"]
+        if @guitar.user_id != session["user_id"]
             redirect "/guitars"
         end
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
     has_many :guitars
     has_secure_password
+    validates :email, uniqueness: true
 end
